220f09202b1d87f26dbf2b9794e9436e6983e716
jcenturion-okta <javier.centurion@okta.com>
2024-03-12 11:58:20 -0300

    update organization-switcher

--- components/a0/organization-switcher.tsx
+++ components/a0/organization-switcher.tsx
@@ -43,12 +43,14 @@ export enum OrganizationTypeOfUsers {
   Allow = "allow",
 }
 
+type SubtitleHandler = string | ((organization: any) => string);
+
 interface OrganizationSwitcherProps extends PopoverTriggerProps {
   user: Claims;
   organizationsClaim: string;
   loginUrl?: string;
   typeOfUsers?: OrganizationTypeOfUsers;
-  subtitle?: string;
+  subtitle?: SubtitleHandler;
 
   showAvatar?: boolean;
   showBorder?: boolean;
@@ -88,6 +90,7 @@ export default function OrganizationSwitcher({
     {
       label: organizationsLabel,
       organizations: user[organizationsClaim].map((org: A0Organization) => ({
+        type: "organization",
         label: org.display_name,
         value: org.id,
         picture: org.picture,
@@ -137,7 +140,9 @@ export default function OrganizationSwitcher({
               <span className="text-sm">{selectedOrg.label}</span>
               {subtitle && (
                 <span className="text-gray-500 font-light text-xs">
-                  {subtitle}
+                  {typeof subtitle === "string"
+                    ? subtitle
+                    : subtitle(selectedOrg)}
                 </span>
               )}
             </div>
@@ -152,8 +157,8 @@ export default function OrganizationSwitcher({
               <path
                 d="M4.93179 5.43179C4.75605 5.60753 4.75605 5.89245 4.93179 6.06819C5.10753 6.24392 5.39245 6.24392 5.56819 6.06819L7.49999 4.13638L9.43179 6.06819C9.60753 6.24392 9.89245 6.24392 10.0682 6.06819C10.2439 5.89245 10.2439 5.60753 10.0682 5.43179L7.81819 3.18179C7.73379 3.0974 7.61933 3.04999 7.49999 3.04999C7.38064 3.04999 7.26618 3.0974 7.18179 3.18179L4.93179 5.43179ZM10.0682 9.56819C10.2439 9.39245 10.2439 9.10753 10.0682 8.93179C9.89245 8.75606 9.60753 8.75606 9.43179 8.93179L7.49999 10.8636L5.56819 8.93179C5.39245 8.75606 5.10753 8.75606 4.93179 8.93179C4.75605 9.10753 4.75605 9.39245 4.93179 9.56819L7.18179 11.8182C7.35753 11.9939 7.64245 11.9939 7.81819 11.8182L10.0682 9.56819Z"
                 fill="currentColor"
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
               ></path>
             </svg>
           </Button>
