f3f2d7a85e7b13b33b1825947191074fe07ee7db
Cristian Douce <cristian@okta.com>
2024-08-23 19:55:29 -0300

    Add explanation about sessions and OIDC Backchannel Logout

--- app/docs/components/user-sessions/page.tsx
+++ app/docs/components/user-sessions/page.tsx
@@ -1,5 +1,7 @@
+import { AlertCircle } from "lucide-react";
 import Balancer from "react-wrap-balancer";
 
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import Code from "@/components/www/code";
 import { DocsLayout } from "@/components/www/layouts";
 import PageLayout from "@/components/www/page-layout";
@@ -19,9 +21,79 @@ export default function UserProfile() {
         title="User Sessions"
         description="Allows the user to review and manage their active sessions"
       >
+        <p className="mb-3 leading-7">
+          It is important to remember when working with Authorization Servers
+          and Web Applications that there is always more than one session
+          involved in the process of tracking the authentication state of a
+          user.
+        </p>
+
+        <p className="mb-3 leading-7">
+          Logging users out implies invalidating all sessions related to this
+          authentication state. It is beacuse of this that the{" "}
+          <a
+            href="https://auth0.com/docs/authenticate/login/logout/back-channel-logout"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-700 hover:underline"
+          >
+            OIDC Back-channel Logout
+          </a>{" "}
+          comes to play an important role to guarantee safely invalidating all
+          user&apos;s active sessions.
+        </p>
+
+        <p className="mb-3 leading-7">
+          You can learn out more about the topic of logging users out safely at:
+        </p>
+        <div>
+          <ul className="list-disc ml-4">
+            <li className="leading-7">
+              <a
+                href="https://auth0.com/blog/the-not-so-easy-art-of-logging-out/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-700 hover:underline"
+              >
+                Auth0 Blog: The Not-So-Easy Art of Logging Out
+              </a>
+            </li>
+            <li className="leading-7">
+              <a
+                href="https://auth0.com/docs/authenticate/login/logout/back-channel-logout"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-700 hover:underline"
+              >
+                Auth0 Docs: OIDC Back-channel Logout
+              </a>
+            </li>
+          </ul>
+        </div>
         <DocTabs align="start" code={componentUsage}>
           <Example />
         </DocTabs>
+
+        <Alert variant="warning" className="mb-10">
+          <AlertCircle className="h-4 w-4 mt-1" />
+          <AlertTitle className="mb-3 mt-1">Important</AlertTitle>
+          <AlertDescription className="text-md">
+            For the &quot;Sign out&quot; to work as expected, it is necessary to
+            implement{" "}
+            <a
+              href="https://auth0.com/docs/authenticate/login/logout/back-channel-logout"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-700 hover:underline"
+            >
+              OIDC Back-channel Logout
+            </a>
+            . If not done so, the only session that will be removed is the one
+            from the Authorization Server (your Auth0 tenant) but your Web
+            Applicaition&apos;s session will remain for as long as its TTL has
+            been defined.
+          </AlertDescription>
+        </Alert>
 
         <h2
           className="font-heading mt-12 scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight first:mt-0"
@@ -36,7 +108,6 @@ export default function UserProfile() {
           </a>
           Installation
         </h2>
-
         <div className="steps mb-12 ml-4 border-l pl-8 [counter-reset:step]">
           <h3 className="font-heading mt-8 scroll-m-20 text-md font-semibold tracking-tight">
             Install the following dependencies:
@@ -97,7 +168,6 @@ export default function UserProfile() {
             Update the import paths to match your project setup.
           </h3>
         </div>
-
         <h2
           className="font-heading mt-12 scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight first:mt-0"
           id="react-hooks"
@@ -111,7 +181,6 @@ export default function UserProfile() {
           </a>
           React Hooks
         </h2>
-
         <div className=" mb-12">
           {componentHooks.map((hook, index) => (
             <div key={index}>
@@ -125,7 +194,6 @@ export default function UserProfile() {
             </div>
           ))}
         </div>
-
         <h2
           className="font-heading mt-12 scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight first:mt-0"
           id="nextjs-routers"
@@ -139,7 +207,6 @@ export default function UserProfile() {
           </a>
           NextJS routers
         </h2>
-
         <div className=" mb-12">
           {componentRoutes.map((route, index) => (
             <div key={index}>
